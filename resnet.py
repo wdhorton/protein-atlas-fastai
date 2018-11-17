06c1d5e374f10dddd92cfefe5ae11f873280ea0f
William Horton <wdt.horton@gmail.com>
2018-11-17 15:25:33 -0500

    Update to add notebook with datablock API.

--- resnet.py
+++ resnet.py
@@ -15,26 +15,26 @@ class Resnet4Channel(nn.Module):
     def __init__(self, encoder_depth=34, pretrained=True, num_classes=28):
         super().__init__()
 
-        self.encoder = RESNET_ENCODERS[encoder_depth](pretrained=pretrained)
+        encoder = RESNET_ENCODERS[encoder_depth](pretrained=pretrained)
         
         # we initialize this conv to take in 4 channels instead of 3
         # we keeping corresponding weights and initializing new weights with zeros
         # this trick taken from https://www.kaggle.com/iafoss/pretrained-resnet34-with-rgby-0-460-public-lb
-        w = self.encoder.conv1.weight
+        w = encoder.conv1.weight
         self.conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
         self.conv1.weight = nn.Parameter(torch.cat((w,torch.zeros(64,1,7,7)),dim=1))
         
-        self.bn1 = self.encoder.bn1
+        self.bn1 = encoder.bn1
         self.relu = nn.ReLU(inplace=True) 
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.layer1 = self.encoder.layer1
-        self.layer2 = self.encoder.layer2
-        self.layer3 = self.encoder.layer3
-        self.layer4 = self.encoder.layer4
+        self.layer1 = encoder.layer1
+        self.layer2 = encoder.layer2
+        self.layer3 = encoder.layer3
+        self.layer4 = encoder.layer4
         
-        self.avgpool = self.encoder.avgpool
+        self.avgpool = encoder.avgpool
         self.fc = nn.Linear(512 * (1 if encoder_depth==34 else 4), num_classes)
         
     def forward(self, x):
